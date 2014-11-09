fbd7e5a1aa209f0d39f09dc8d94d7a4be8fe37aa
Jordi Esteve <jesteve@zikzakmedia.com>
2014-11-09 12:30:18 +0100

    PEP8 fixes

--- survey.py
+++ survey.py
@@ -2,9 +2,10 @@
 # This file is part of the survey module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelSingleton, ModelSQL, ModelView, DictSchemaMixin, fields
+from trytond.model import ModelSingleton, ModelSQL, ModelView, \
+    DictSchemaMixin, fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval, Bool
+from trytond.pyson import Eval
 
 __all__ = ['Configuration', 'Survey', 'SurveyField']
 __metaclass__ = PoolMeta
